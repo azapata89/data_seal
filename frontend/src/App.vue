032a20d037c8e16cf1ee23208034ff0ef7424296
Andres Zapata <andreszapata89@gmail.com>
2024-12-19 01:06:42 -0500

    inicio

---
+++ frontend/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h1>Bienvenido a DataSeal</h1>
+    <p>Haz clic en el botón de abajo para empezar a firmar.</p>
+    <button @click="navigateToForm">Ir al Formulario</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    navigateToForm() {
+      console.log('Aquí puedes redirigir a un formulario.');
+    },
+  },
+};
+</script>
